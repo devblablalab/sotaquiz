4aa35861530d4a60b07cbe2c977098ecac1eaa17
Karine <karinemsoliveira@gmail.com>
2023-10-17 22:13:07 -0300

    Result messages

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -10,15 +10,28 @@ import { QuizService } from 'src/app/services/quiz.service';
 export class ResultComponent implements OnInit {
   public correctAnswers : QuizCorrectQuestions[] | [] = [];
   public ufList : any = [];
+  public resultImage : string = "";
   constructor(private service : QuizService) { }
 
   async ngOnInit() {
     this.service.protectQuizRoute();
     this.ufList = await this.service.setDataUfs();
     this.correctAnswers = this.service.getCorrectAnswers();
+    this.resultImage = this.getResultImage();
+
   }
 
   public checkUfItemIsCorrect(uf : string) {
     return this.correctAnswers.find(answer => answer.answerValue === uf.toLowerCase()) !== undefined;
+  }
+
+  public getResultImage() {
+    if(this.correctAnswers.length < 10){
+      return "/assets/shapes/bad-result.svg";
+    } else if(this.correctAnswers.length < 20) {
+      return "/assets/shapes/medium-result.svg";
+    } else {
+      return "/assets/shapes/good-result.svg";
+    }
   }
 }
