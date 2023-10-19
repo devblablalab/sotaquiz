b60d6aa27349ca4feba80380757a656508bd8224
devblablalab <devblablalab@gmail.com>
2023-10-18 23:40:12 -0300

    New Header Quiz

--- src/app/components/header-quiz/header-quiz.component.ts
+++ src/app/components/header-quiz/header-quiz.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
   selector: 'app-header-quiz',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-quiz.component.scss']
 })
 export class HeaderQuizComponent {
+  @Input() questionsLength!: number;
+  constructor(public quizService: QuizService) {}
 
+  public formatQuestionNumber(currentQuestion : number) {
+    return currentQuestion.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  }
 }
